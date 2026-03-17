2d4f6224f8cf0d7748fbb5d8990532029b1cd900
mj24-brand <jjyo3162@gmail.com>
2026-03-17 14:26:36 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const connectDB=async ()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("mongoDB connected");
+    }catch(error){
+        console.error("database connection failed",error);
+        process.exit(1);
+    }
+};
+export default connectDB;
